2d4365c3536ecd6e3189ae1b84780f493f4349c3
kenneth david <kennethdavid256.com>
2026-02-08 22:27:04 +0300

    i do not know

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig, env } from 'prisma/config';
 
-if (process.env.VERCEL !== '1') {
-  require('dotenv/config');
-}
+require('dotenv/config');
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
-    url: env('DIRECT_URL') || env('DATABASE_URL'),
+    url: env('DATABASE_URL'),
   },
 });
