231a41ba9912396733e52657a0ea15e169342b77
kenneth david <kennethdavid256.com>
2026-02-08 19:43:54 +0300

    fixed config

--- app/api/lib/prisma.ts
+++ app/api/lib/prisma.ts
@@ -1,10 +1,22 @@
 import { PrismaClient } from '@prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const prismaClientSingleton = () => {
+ 
+  const options: any = {
+    accelerateUrl: process.env.ACCELERATE_URL || process.env.DATABASE_URL,
+    log: ['query', 'error', 'warn'],
+  };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({
-  log: ['query', 'error', 'warn'],
-}).$extends(withAccelerate());
+  return new PrismaClient(options).$extends(withAccelerate());
+};
+
+type PrismaClientConfigured = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = global as unknown as { 
+  prisma: PrismaClientConfigured | undefined 
+};
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
