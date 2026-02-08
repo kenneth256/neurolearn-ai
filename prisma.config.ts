65aebcb7baa6d780fb954aac0b3a293899cc963d
kenneth david <kennethdavid256.com>
2026-02-08 21:12:53 +0300

    fixed dotenv dependencies

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,9 @@
-import { defineConfig } from 'prisma/config';
-import { config } from 'dotenv';
-
-
-config();
+import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
-    url: process.env.DIRECT_URL || process.env.DATABASE_URL || '',
+    
+    url: env('DATABASE_URL'),
   },
 });
